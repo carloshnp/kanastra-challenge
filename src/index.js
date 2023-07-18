d726d4dd123893ef3fc37a417d58ac097d2003f4
carloshnp <carloshnpwork@gmail.com>
2023-07-17 22:06:09 -0300

    feat: express.json + payments route

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import { debtsRouter } from "./routers/debts-router.js";
+import { payloadRouter } from "./routers/payload-router.js";
 
 const app = express();
 
@@ -11,7 +12,9 @@ const port = process.env.PORT;
 
 app
   .use(cors())
+  .use(express.json())
   .use("/debts", debtsRouter)
+  .use("/payments", payloadRouter)
   .get("/health", (_req, res) => res.send("OK!"));
 
 app.listen(port, () => {
