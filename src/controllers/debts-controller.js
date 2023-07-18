a96df4d95d9b88090f28f9e7d431451f166df9e2
carloshnp <carloshnpwork@gmail.com>
2023-07-17 22:58:29 -0300

    feat: check if all rows are in the csv file

--- src/controllers/debts-controller.js
+++ src/controllers/debts-controller.js
@@ -2,14 +2,20 @@ import { createDebts } from "../services/debts-service.js";
 import { getDebtsAndPayments } from "../services/debts-service.js";
 
 async function saveCsvToDatabase(req, res) {
-    try {
-        const row = req.rowData;
-        const createdDebts = await createDebts(row);
-        return res.sendStatus(201);
-    } catch (error) {
-        console.log('Error creating debt');
-        return res.sendStatus(500);
+  try {
+    const rows = req.rowData;
+    for (const row of rows) {
+      if (!isValidRow(row)) {
+        return res.status(400).send({ error: 'Invalid CSV data' });
+      }
     }
+
+    const createdDebts = await createDebts(rows);
+    return res.sendStatus(201);
+  } catch (error) {
+    console.log('Error creating debt:', error);
+    return res.sendStatus(500);
+  }
 }
 
 async function getDebtsWithPayments(req, res) {
@@ -17,8 +23,20 @@ async function getDebtsWithPayments(req, res) {
     const debtsWithPayments = await getDebtsAndPayments();
     return res.json(debtsWithPayments);
   } catch (error) {
-    console.log(error);
+    console.log('Error fetching debts with payments:', error);
+    return res.sendStatus(500);
   }
+}
+
+function isValidRow(row) {
+  const requiredFields = ['name', 'governmentId', 'email', 'debtAmount', 'debtDueDate', 'debtId'];
+
+  for (const field of requiredFields) {
+    if (!(field in row)) {
+      return false;
+    }
+  }
+  return true;
 }
 
 export { saveCsvToDatabase, getDebtsWithPayments };
